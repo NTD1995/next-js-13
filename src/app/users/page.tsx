7c3d5cc413ef324b6786574ab0733db3846b6d23
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2024-09-09 20:19:21 +0900

    next-js-13修正

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,5 +1,5 @@
-import UserList from './UserList';
-import { Suspense } from 'react';
+import UserList from "./UserList";
+import { Suspense } from "react";
 
 type User = {
   id: string;
@@ -9,15 +9,15 @@ type User = {
 
 const Page = async () => {
   await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: User[] = await response.json();
   console.log(users);
   return (
     <div className="m-4">
       <h1 className="text-lg font-bold">ユーザ一覧</h1>
-         <Suspense fallback={<p>Loading...</p>}></Suspense>
+      <Suspense fallback={<p>Loading...</p>}></Suspense>
 
-         <UserList />
+      <UserList />
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
@@ -26,6 +26,5 @@ const Page = async () => {
     </div>
   );
 };
-
 
 export default Page;
