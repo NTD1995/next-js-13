618b4dea421bac5e8776be57856bbd24820d5980
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2024-09-17 23:26:33 +0900

    next-js-13修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import Link from 'next/link';
-import Counter from './Counter';
+import Link from "next/link";
+import Counter from "./Counter";
 
 export default function Home() {
   return (
     <div className="m-4">
-   <Link href="/about" className="underline" prefetch={false}>
+      <Link href="/about" className="underline" prefetch={false}>
         About
       </Link>
       <h1 className="text-2xl">Home</h1>
+      <Counter />
     </div>
   );
 }
