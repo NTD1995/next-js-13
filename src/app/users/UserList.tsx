868dc5e72ebc42392bf10dfd2300e3053c2b1fef
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2024-10-01 01:33:07 +0900

    next-js-13修正

--- src/app/users/UserList.tsx
+++ src/app/users/UserList.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
+import { NextResponse } from "next/server";
+import { headers, cookies } from "next/headers";
 
 type User = {
   id: string;
@@ -19,5 +21,26 @@ const UserList = async () => {
     </ul>
   );
 };
+
+export async function GET() {
+  const headersList = headers();
+  const cookieStore = cookies();
+
+  console.log("headersList", headersList);
+  console.log("cookieStore", cookieStore);
+
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  return NextResponse.json(data);
+}
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const name = searchParams.get("name");
+  console.log(name);
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  return NextResponse.json(data);
+}
 
 export default UserList;
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,48 +1,18 @@
 import Link from "next/link";
 import Counter from "./counter";
-import UserList from "./UserList";
-import { Suspense } from "react";
+import UserList from "./users/UserList";
 
 export default function Home() {
   return (
     <div className="m-4">
-      <Link href="/about" className="underline">
+      <Link href="/about" className="underline" prefetch={false}>
         About
       </Link>
       <h1 className="text-2xl">Home</h1>
       <Counter>
         <h2 className="font-bold text-lg mt-4">ユーザ一覧</h2>
-        {/* @ts-expect-error Async Server Component */}
         <UserList />
       </Counter>
     </div>
   );
 }
-
-type User = {
-  id: string;
-  name: string;
-  email: string;
-};
-
-const Page = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users: User[] = await response.json();
-  console.log(users);
-  return (
-    <div className="m-4">
-      <h1 className="text-lg font-bold">ユーザ一覧</h1>
-      <Suspense fallback={<p>Loading...</p>}>
-        <UserList />
-        <ul>
-          {users.map((user) => (
-            <li key={user.id}>{user.name}</li>
-          ))}
-        </ul>
-      </Suspense>
-    </div>
-  );
-};
-
-export default Page;
