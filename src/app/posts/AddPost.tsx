f0e1b11c5e166e7d5f0e7b1d8fa1ac531b6c1135
安部大貴 <76642241+NTD1995@users.noreply.github.com>
2024-10-14 00:14:18 +0900

    next-js-13修正

--- src/app/posts/AddPost.tsx
+++ src/app/posts/AddPost.tsx
@@ -1,6 +1,9 @@
+"use client";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function AddPost() {
+  const router = useRouter();
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const handleSubmit: React.FormEventHandler<HTMLFormElement> = async (
@@ -18,6 +21,8 @@ export default function AddPost() {
 
     setTitle("");
     setContent("");
+
+    router.refresh();
   };
 
   return (
