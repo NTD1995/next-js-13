6beb8f4fabbd3a294e0d2c85d3daed23f6b545da
Shogo Wada <aiz4338150@gmail.com>
2024-10-17 08:39:43 +0900

    インポートエラーの修正

--- src/app/users/UserList.tsx
+++ src/app/users/UserList.tsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
-import { NextResponse } from "next/server";
-import { headers, cookies } from "next/headers";
 
-type User = {
+export type User = {
   id: string;
   name: string;
   email: string;
@@ -22,20 +20,5 @@ const UserList = async () => {
     </ul>
   );
 };
-
-export async function GET() {
-  const headersList = headers();
-  const cookieStore = cookies();
-
-  console.log("headersList", headersList);
-  console.log("cookieStore", cookieStore);
-
-  const response = await fetch("http://localhost:3000/api", {
-    // cache: 'no-store',
-    next: { revalidate: 5 },
-  });
-  const data = await response.json();
-  return NextResponse.json(data);
-}
 
 export default UserList;
